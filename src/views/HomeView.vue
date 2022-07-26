aea05e3f107a67420c137da55efa4788367762b4
Ikhwanwp <wiratama900@gmail.com>
2022-07-26 10:10:46 +0700

    -

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderStore />
+    <HeroStore />
+    <ProductStore />
+    <SosmedStore />
+    <PartnerStore />
+    <FooterStore />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderStore from "@/components/HeaderStore.vue";
+import FooterStore from "@/components/FooterStore.vue";
+import HeroStore from "@/components/HeroStore.vue";
+import ProductStore from "@/components/ProductStore.vue";
+import SosmedStore from "../components/SosmedStore.vue";
+import PartnerStore from "../components/PartnerStore.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderStore,
+    HeroStore,
+    FooterStore,
+    ProductStore,
+    SosmedStore,
+    PartnerStore,
+  },
+};
+</script>
